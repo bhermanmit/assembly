229e86567109414985c6216b04d7d8cb950869fc
Bryan Herman <bherman@mit.edu>
2014-01-29 16:35:16 -0500

    add base scripts

---
+++ assembly.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python2
+
+surf_dict = {}
+
+class Nuclide(object):
+    def __init__(self, name, xs, value):
+        self.name = name
+        self.xs = xs
+        self.value = value
+
+class Sab(object):
+    def __init__(self, name, value):
+        self.name = name
+        self.xs = xs
+
+class Material(object):
+    n_materials = 0
+    def __init__(self, nuclides, sab = None):
+        self.n_materials += 1
+        self.material_id = n_materials
+        self.nuclides = nuclides
+        self.sab = sab
+
+class Surface(object):
+    n_surfaces = 0
+    def __init__(self, type, coeffs = "", comment=None):
+        self.n_surfaces += 1
+        self.surface_id = self.n_surfaces
+        self.type = type
+        self.coeffs = coeffs
+        self.comment = comment
+
+class Cell(object):
+    n_cells = 0
+    def __init__(self, surfaces, universe=None, fill=None, material=None, comment=None):
+        self.n_cells += 1
+        self.cell_id = self.n_cells
+        self.universe = universe
+        self.material = material
+        self.surfaces = surfaces
+        self.comment = comment
+        self.checked = False
+
+        # check cell setup
+        self.checked = self.check_cell()
+        if not self.checked:
+            raise Exception('Cell needs a universe and fill or material!')
+
+    def check_cell(self):
+        if self.universe == None:
+            return False
+        if self.fill == None or self.material == None:
+            return False
+
+class Lattice(object):
+    n_lattices = 0
+    def __init__(self, dimension, lower_left, upper_right, universes):
+        self.n_lattices += 1
+        self.lattice_id = self.n_lattices
+        self.type = "rectangular"
+        self.dimension = dimension
+        self.lower_left = lower_left
+        self.upper_right = upper_right
+        self.universes = universes
+
+        # Get lattice dimension
+        self.nx = dimension.split()[0]
+        self.ny = dimension.split()[1]
+
+def add_surface(key, type, coeffs, comment=None):
+     surf_dict.update({key:Surface(type, coeffs, comment)})
---
+++ make_assembly.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2
+
+from assembly import *
+
+# Create fuel pin surfaces
+add_surface('fuelOR', 'z-cylinder', '0.0 0.0 0.392180', 'Fuel Outer Radius')
+
